0b146173076b9f588090c042515225811b7dbcd3
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-05-31 15:07:56 +0200

    Create courses.js

---
+++ mock/courses.js
@@ -0,0 +1,18 @@
+const courses = [
+  {
+    id: 1,
+    title: "Introducción a Blockchain",
+    duration: "2 horas",
+    xolidReward: 300,
+    completed: false
+  },
+  {
+    id: 2,
+    title: "Finanzas Descentralizadas",
+    duration: "3 horas",
+    xolidReward: 450,
+    completed: true
+  }
+];
+
+export default courses;
