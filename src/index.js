c0e4c18d0364385fada066daf5cc2e67dbe8fc1b
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-05-31 16:04:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import App from "./App";
-
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
