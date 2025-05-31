5ea81d716d61559a9e6cc7d7c5a7a2f138c73f0d
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-05-31 16:05:22 +0200

    Update index.css

--- src/index.css
+++ src/index.css
@@ -1,11 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
