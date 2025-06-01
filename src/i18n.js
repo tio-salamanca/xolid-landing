ee5b040a5c9af12907eff1bab134c58666933ac8
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-06-01 11:21:35 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,28 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-import es from './locales/es/translation.json';
 import en from './locales/en/translation.json';
+import es from './locales/es/translation.json';
 import de from './locales/de/translation.json';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      es: { translation: es },
       en: { translation: en },
-      de: { translation: de }
+      es: { translation: es },
+      de: { translation: de },
     },
-    lng: 'es',
     fallbackLng: 'es',
-    interpolation: { escapeValue: false }
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+    },
   });
 
 export default i18n;
