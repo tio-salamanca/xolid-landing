35772d1fa832695c12959eca37badecc749827f2
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-06-01 09:13:21 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,12 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'es',
+    fallbackLng: "es",
     resources: {
       es: {
         translation: {
@@ -14,7 +14,11 @@ i18n
           education: "Educación",
           solidarity: "Solidaridad",
           actions: "Mis Acciones",
-          language: "Idioma",
+          learn_and_earn: "Aprende y gana XOLID",
+          complete_courses: "Completa cursos y quizzes para acumular tokens",
+          solidary_actions: "Acciones Solidarias",
+          join_causes: "Participa en causas sociales y gana recompensas",
+          your_actions_history: "Tu Historial de Acciones"
         }
       },
       en: {
@@ -23,7 +27,11 @@ i18n
           education: "Education",
           solidarity: "Solidarity",
           actions: "My Actions",
-          language: "Language",
+          learn_and_earn: "Learn and earn XOLID",
+          complete_courses: "Complete courses and quizzes to earn tokens",
+          solidary_actions: "Solidary Actions",
+          join_causes: "Join social causes and earn rewards",
+          your_actions_history: "Your Actions History"
         }
       },
       de: {
@@ -32,12 +40,13 @@ i18n
           education: "Bildung",
           solidarity: "Solidarität",
           actions: "Meine Aktionen",
-          language: "Sprache",
+          learn_and_earn: "Lerne und verdiene XOLID",
+          complete_courses: "Absolviere Kurse und Quizze, um Token zu sammeln",
+          solidary_actions: "Solidarische Aktionen",
+          join_causes: "Nimm an sozialen Aktionen teil und verdiene Belohnungen",
+          your_actions_history: "Deine Aktionshistorie"
         }
       }
-    },
-    interpolation: {
-      escapeValue: false
     }
   });
 
