0530cb4b6e8a05a4d89c2287e8cf046aa698b8f8
CjCL <31179263+tio-salamanca@users.noreply.github.com>
2025-06-01 11:12:44 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -12,7 +11,6 @@ const firebaseConfig = {
   measurementId: "G-3GJZMNYP4N"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
 export const db = getFirestore(app);
